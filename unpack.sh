020271038845d486d682e92ce36d752b9020ad1b
Munken <mm.munk@gmail.com>
2017-04-16 19:07:25 +0200

    OUTPUTPUT -> OUTPUT

--- unpack.sh
+++ unpack.sh
@@ -2,5 +2,5 @@ for i in ${INPUT_DIR}/*.lmd.gz
 do
     filename=$(basename "$i")
     f="${filename%%.*}"
-    ${UNPACKER} $i --ntuple=RAW,${OUTPUTPUT_DIR}/$f.root
+    ${UNPACKER} $i --ntuple=RAW,${OUTPUT_DIR}/$f.root
 done
